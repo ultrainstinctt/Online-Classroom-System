cb3d36ad28dcc33379d2e401eaf5017caf03379a
Pratap Bhattacharjee <85588285+ultrainstinctt@users.noreply.github.com>
2023-10-26 21:59:34 +0530

    Delete Online Classroom System directory

--- Online Classroom System/FACALTY/logout.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php
-    if (!isset($_SESSION)) { session_start(); }
-    $_SESSION = array(); 
-    session_destroy(); 
-     header("Location:../index.php"); // Or wherever you want to redirect
-    exit();
-?>
